e534a609a64d391c8ea4849a482e008b52f0df3e
Pregum <pregum.y@gmail.com>
2020-02-10 23:45:05 +0900

    [add] 引数用のNodeクラスを作成しINodeから派生したIOPerationalNodeを作成

---
+++ UmlCreator.Core/Diagram/ArgumentNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UmlCreator.Core.Diagram
+{
+    internal class ArgumentNode : INode
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// 型
+        /// </summary>
+        public string Type { get; }
+
+        /// <summary>
+        /// アクセスレベル
+        /// </summary>
+        public AccessLevel Accessibility => AccessLevel.Private;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="name">名前</param>
+        /// <param name="type">型</param>
+        public ArgumentNode(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
---
+++ UmlCreator.Core/Diagram/IOperationalNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UmlCreator.Core.Diagram
+{
+    /// <summary>
+    /// 操作可能なノードを表します
+    /// </summary>
+    public interface IOperationalNode : INode
+    {
+        /// <summary>
+        /// 引数リスト
+        /// </summary>
+        IReadOnlyList<INode> Arguments { get; }
+
+        /// <summary>
+        /// 引数リストを持っているかどうか
+        /// </summary>
+        bool HasArguments { get => Arguments?.Any() ?? false; }
+
+    }
+}
