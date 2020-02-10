3b555d8ee93f5580a179e8cbc1d9cb98002c8078
Pregum <pregum.y@gmail.com>
2020-02-11 05:44:17 +0900

    [add] テストの追加

---
+++ UmlCreator.Core.Test/CommandLineTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UmlCreator.Core.Parser;
+using Xunit;
+
+namespace UmlCreator.Core.Test
+{
+    public class CommandLineTest
+    {
+        [Theory(DisplayName ="入力ファイルパスのテスト")]
+        [InlineData(@"C:\tmp\input_test.pu -o C:\tmp\out")]
+        public void InputTest1(string inputPath)
+        {
+            var parser = CommandParser.Parse(inputPath);
+            Assert.Equal(@"C:\tmp\input_test.pu", parser.InputPath);
+        }
+
+        [Theory(DisplayName ="入力ファイルパスの半角スペーステスト")]
+        [InlineData(@"""C:\tmp\input test.pu"" -o C:\tmp\out")]
+        public void InputTest2(string inputPath)
+        {
+            var parser = CommandParser.Parse(inputPath);
+            Assert.Equal(@"C:\tmp\input test.pu", parser.InputPath);
+        }
+
+        [Theory(DisplayName ="入力ファイルパスの記号テスト")]
+        [InlineData(@"C:\tmp\input-test.pu -o C:\tmp\out")]
+        public void InputTest3(string inputPath)
+        {
+            var parser = CommandParser.Parse(inputPath);
+            Assert.Equal(@"C:\tmp\input-test.pu", parser.InputPath);
+        }
+
+        [Theory(DisplayName ="入力ファイルパスの記号テスト2")]
+        [InlineData(@"C:\tmp\..\input_test.pu -o C:\tmp\out")]
+        public void InputTest4(string inputPath)
+        {
+            var parser = CommandParser.Parse(inputPath);
+            Assert.Equal(@"C:\tmp\..\input_test.pu", parser.InputPath);
+        }
+
+        [Theory(DisplayName ="入力ファイルの相対パステスト1")]
+        [InlineData(@"tmp\input_test.pu -o C:\tmp\out")]
+        public void RelativeInputPathTest1(string inputPath)
+        {
+            var parser = CommandParser.Parse(inputPath);
+            Assert.Equal(@"tmp\input_test.pu", parser.InputPath);
+        }
+
+        [Theory(DisplayName ="出力パスのテスト1")]
+        [InlineData(@"tmp\input_test.pu -o C:\tmp\out")]
+        public void OutputPathTest1(string val)
+        {
+            var parser = CommandParser.Parse(val);
+            Assert.Equal(@"C:\tmp\out", parser.OutputPath);
+        }
+
+        [Theory(DisplayName ="出力パスのテスト2")]
+        [InlineData(@"tmp\input_test.pu -o C:\tmp\out  ")]
+        public void OutputPathTest2(string val)
+        {
+            var parser = CommandParser.Parse(val);
+            Assert.Equal(@"C:\tmp\out", parser.OutputPath);
+        }
+    }
+}
---
+++ UmlCreator.Core/Parser/CommandParser.cs
@@ -0,0 +1,69 @@
+﻿using Sprache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UmlCreator.Core.Parser
+{
+    public class CommandParser
+    {
+
+        public string InputPath { get; private set; }
+        public string OutputPath { get; private set; }
+        public OutputType OutputType { get; private set; }
+
+        private readonly static Parser<string> NormalInputParser =
+            from input in Sprache.Parse.CharExcept(' ').AtLeastOnce().Token().Text()
+            select input;
+
+        private readonly static Parser<string> QuoteInputParser =
+            from openQuote in Sprache.Parse.Char('"')
+            from input in Sprache.Parse.CharExcept('"').AtLeastOnce().Token().Text()
+            from closeQuote in Sprache.Parse.Char('"')
+            select input;
+
+        /// <summary>
+        /// 入力パスのパーサー
+        /// </summary>
+        private readonly static Parser<string> InputParser =
+            QuoteInputParser.Or(NormalInputParser);
+
+        private readonly static Parser<string> NormalOutputParser =
+            from output in Sprache.Parse.CharExcept(' ').AtLeastOnce().Token().Text()
+            select output;
+
+        private readonly static Parser<string> QuoteOutputParser =
+            from openQuote in Sprache.Parse.Char('"')
+            from output in Sprache.Parse.CharExcept('"').AtLeastOnce().Token().Text()
+            from closeQuote in Sprache.Parse.Char('"')
+            select output;
+
+        /// <summary>
+        /// 出力パスのパーサー
+        /// </summary>
+        private readonly static Parser<string> OutputParser =
+            QuoteOutputParser.Or(NormalOutputParser);
+
+        private readonly static Parser<CommandParser> Parser =
+            from input in InputParser
+            from output in Sprache.Parse.Regex("-o").Then(x => OutputParser)
+            select new CommandParser(input, output);
+
+
+        public static CommandParser Parse(string input)
+        {
+            var result = Parser.Parse(input);
+            return result;
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public CommandParser(string input, string output)
+        {
+            InputPath = input;
+            OutputPath = output;
+        }
+    }
+}
