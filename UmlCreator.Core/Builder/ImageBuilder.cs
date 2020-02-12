f443a8f7802fc0bc1ebf1c683581b9978ed9c484
Pregum <pregum.y@gmail.com>
2020-02-13 06:53:38 +0900

    [update] 出力する型をBitmapImageからBitmapに変更

--- UmlCreator.Core/Builder/ImageBuilder.cs
+++ UmlCreator.Core/Builder/ImageBuilder.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 using System.Windows.Media.Imaging;
 using UmlCreator.Core.Diagram;
 
 namespace UmlCreator.Core.Builder
 {
-    internal class ImageBuilder : IBuilder<BitmapImage>
+    internal class ImageBuilder : IBuilder<Bitmap>
     {
         /// <summary>
         /// BitmapImageでクラス図を生成します。
         /// </summary>
         /// <param name="rootNode">根本のノード</param>
         /// <returns>クラス図</returns>
-        public BitmapImage MakeDiagram(IRootNode rootNode)
+        Bitmap IBuilder<Bitmap>.MakeDiagram(IRootNode rootNode)
         {
             throw new NotImplementedException();
         }
