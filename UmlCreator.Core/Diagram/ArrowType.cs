6d42dc3e13ed88e29585be69effa598b79f0ca3c
Pregum <pregum.y@gmail.com>
2020-02-13 21:46:34 +0900

    [add] EdgeNodeとClassNodeを同一観する為のinterfaceを追加

---
+++ UmlCreator.Core/Diagram/ArrowType.cs
@@ -0,0 +1,25 @@
+﻿namespace UmlCreator.Core.Diagram
+{
+    /// <summary>
+    /// 矢印の種類
+    /// </summary>
+    internal enum ArrowType
+    {
+        /// <summary>
+        /// なし
+        /// </summary>
+        None,
+        /// <summary>
+        /// 依存
+        /// </summary>
+        Dependency,
+        /// <summary>
+        /// 継承
+        /// </summary>
+        Extend,
+        /// <summary>
+        /// 実装
+        /// </summary>
+        Implement,
+    }
+}
---
+++ UmlCreator.Core/Diagram/EdgeNode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UmlCreator.Core.Diagram
+{
+    /// <summary>
+    /// エッジを表しているクラス
+    /// </summary>
+    internal class EdgeNode : IExpression
+    {
+        /// <summary>
+        /// 始点側のノード名
+        /// </summary>
+        public string SourceNodeName { get; private set; }
+        /// <summary>
+        /// 終点側のノード名
+        /// </summary>
+        public string TargetNodeName { get; private set; }
+        /// <summary>
+        /// 始点方向の矢印の種類
+        /// </summary>
+        public ArrowType BackArrowType { get; private set; }
+        /// <summary>
+        /// 終点方向の矢印の種類
+        /// </summary>
+        public ArrowType ForwardArrowType { get; private set; }
+
+        // TODO: 線の種類のメンバも追加する
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="leftNodeName"> 左側のノード名 </param>
+        /// <param name="rightNodeName"> 右側のノード名 </param>
+        /// <param name="leftArrowType"> 左方向の矢印の種類</param>
+        /// <param name="rightArrowType"> 右方向の矢印の種類 </param>
+        public EdgeNode(string leftNodeName, string rightNodeName, ArrowType leftArrowType, ArrowType rightArrowType)
+        {
+            BackArrowType = leftArrowType;
+            ForwardArrowType = rightArrowType;
+
+            // 矢印は多くても片方までしか表示させない
+            (SourceNodeName, TargetNodeName) = (leftArrowType, rightArrowType) switch
+            {
+                (ArrowType.None, ArrowType.None) => (leftNodeName, rightNodeName),
+                (ArrowType.None, ArrowType forwardArrow) => (leftNodeName, rightNodeName),
+                (ArrowType backArrow, ArrowType.None) => (rightNodeName, leftNodeName),
+                _ => throw new InvalidOperationException(),
+            };
+        }
+    }
+}
